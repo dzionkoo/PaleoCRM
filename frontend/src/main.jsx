068b503e0fc6984bb9c5736dc84df1c5477098aa
Dzionko <czerwieckonradek@gmail.com>
2026-04-25 17:06:45 +0200

    main.jsx fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // Usuń tę linię, jeśli nie masz jeszcze pliku CSS
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
